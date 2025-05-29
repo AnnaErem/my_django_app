395c3609ff3d0fdb067dc8d9572a8b458d63c5d0
stelmak <liza.stelmak4@gmail.com>
2025-05-29 04:49:29 +0300

    Update apps.py

--- StelmakApp_2/apps.py
+++ StelmakApp_2/apps.py
@@ -1,13 +1,4 @@
 from django.apps import AppConfig
-from flask import Flask, render_template
-import sqlite3 
-
-StelmakApp_2 = Flask(__name__)
-
-@StelmakApp_2.route('/')
-def index():
-    return render_template("index.html")
-
 
 class Stelmakapp2Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
