066548aa57c8c01f6ff39ba9e1509185ef0a32b2
Elisaveta Stelmak <liza.stelmak4@gmail.com>
2025-05-29 04:26:40 +0300

    initial commit

--- StelmakApp_2/apps.py
+++ StelmakApp_2/apps.py
@@ -1,4 +1,12 @@
 from django.apps import AppConfig
+from flask import Flask, render_template
+import sqlite3 
+
+StelmakApp_2 = Flask(__name__)
+
+@StelmakApp_2.route('/')
+def index():
+    return render_template("index.html")
 
 
 class Stelmakapp2Config(AppConfig):
