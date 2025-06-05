727d58fec1e1570e4b8e587a0e30871b7e00aa2b
Sibrian4ik <kosty2004tiger@gmail.com>
2025-06-05 04:59:08 +0300

    12

--- project02/settings.py
+++ project02/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,14 @@ INSTALLED_APPS = [
     "StelmakApp", 
     'StelmakApp_2'
 ]
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'StelmakApp_2/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
